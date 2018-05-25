5c81fb526cf6be118ad4d575ed11c6003012c6ac
James Waller <james.waller1984@gmail.com>
2018-05-26 09:24:24 +1000

    Car and Notice components added. File names corrected.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import TeslaBattery from './containers/TeslaBattery';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <TeslaBattery />
       </div>
     );
   }
---
+++ src/containers/TeslaBattery.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TeslaBattery.css';
+import TeslaCar from '../components/TeslaCar/TeslaCar';
+import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
+
+class TeslaBattery extends React.Component {
+  render() {
+    return (
+      <form className="tesla-battery">
+        <h1>Range Per Charge</h1>
+        <TeslaCar />
+        <TeslaNotice />
+      </form>
+    )
+  }
+}
+export default TeslaBattery;
